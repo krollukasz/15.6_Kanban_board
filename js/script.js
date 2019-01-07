42a21e5e0cb917b95f8354934833c3a3852163a0
Łukasz Król <lionel@o2.pl>
2019-01-07 12:26:44 +0100

    Add function to generate template and column class

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+  function randomString() {
+    var chars = "0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ";
+    var str = ""
+    for (var i = 0; i < 10; i++) {
+      str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+  }
+
+  function generateTemplate(name, data, basicElement) {
+    var template = document.getElementById(name).innerHTML;
+    var element = document.createElement(basicElement || "div");
+
+    Mustache.parse(template);
+    element.innerHTML = Mustache.render(template, data);
+
+    return element;
+  }
+
+  function Column(name) {
+    var self = this;
+
+    this.id = randomString();
+    this.name = name;
+    this.element = generateTemplate("column-template", {name: this.name};)
+  }
+});
