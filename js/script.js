d963e80f3f1dceda12335c6a53c807aa367bcab6
Łukasz Król <lionel@o2.pl>
2019-01-07 15:47:00 +0100

    Add Column prototype and methods

--- js/script.js
+++ js/script.js
@@ -25,6 +25,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
     this.id = randomString();
     this.name = name;
-    this.element = generateTemplate("column-template", {name: this.name};)
+    this.element = generateTemplate("column-template", {
+      name: this.name,
+      id: this.id
+    });
+
+    this.element.querySelector(".column").addEventListener("click", function (event) {
+      if (event.target.classList.contains("btn-delete")) {
+        self.removeColumn();
+      }
+
+      if (event.target.classList.contains("add-card")) {
+        self.addCard(new Card(prompt("Enter card name")));
+      }
+    });
+
+    // Column prototype
+    Column.prototype = {
+      addCard: function(card) {
+        this.element.querySelector("ul").appendChild(card.element);
+      },
+      removeColumn: function() {
+        this.element.parentNode.removeChild(this.element);
+      }
+    };
   }
 });
