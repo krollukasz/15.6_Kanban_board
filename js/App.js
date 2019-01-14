8bd7b65dafe29440787a747f5c8a594a698af19d
Łukasz Król <lionel@o2.pl>
2019-01-14 16:10:13 +0100

    Add function to create columns, remove randomString function

--- js/App.js
+++ js/App.js
@@ -14,30 +14,29 @@ fetch(baseUrl + "/board", { headers: myHeaders })
     setupColumns(resp.columns);
   });
 
-// GENERAL FUNCION
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
+// FUNCTION TO CREATE COLUMNS
+function setupColumns(columns) {
+  columns.forEach(function(column) {
+    var col = new Column(column.id, column.name);
+    board.addColumn(col);
+  });
 }
 
+// FUNCION TO GENERATE TEMPLATE 
 function generateTemplate(name, data, basicElement) {
-  	var template = document.getElementById(name).innerHTML;
-  	var element = document.createElement(basicElement || 'div');
-  
-  	Mustache.parse(template);
-  	element.innerHTML = Mustache.render(template, data);
-  
-  	return element;
+  var template = document.getElementById(name).innerHTML;
+  var element = document.createElement(basicElement || "div");
+
+  Mustache.parse(template);
+  element.innerHTML = Mustache.render(template, data);
+
+  return element;
 }
 
 // CREATE NEW COLUMN
-var todoColumn = new Column('To Do');
-var doingColumn = new Column('Doing');
-var doneColumn = new Column('Done');
+var todoColumn = new Column("To Do");
+var doingColumn = new Column("Doing");
+var doneColumn = new Column("Done");
 
 // ADD COLUMNS TO THE BOARD
 board.createColumn(todoColumn);
@@ -45,8 +44,8 @@ board.createColumn(doingColumn);
 board.createColumn(doneColumn);
 
 // CREATE NEW CARDS
-var card1 = new Card('New task');
-var card2 = new Card('Create Kanban board');
+var card1 = new Card("New task");
+var card2 = new Card("Create Kanban board");
 
 // ADD CARDS TO COLUMNS
 todoColumn.createCard(card1);
