573d45933f918ca7454d4ac791a76fba6979c746
Łukasz Król <lionel@o2.pl>
2019-01-14 21:44:49 +0100

    Remove unnecessary parts of the code

--- js/App.js
+++ js/App.js
@@ -40,21 +40,3 @@ function generateTemplate(name, data, basicElement) {
 
   return element;
 }
-
-// CREATE NEW COLUMN
-var todoColumn = new Column("To Do");
-var doingColumn = new Column("Doing");
-var doneColumn = new Column("Done");
-
-// ADD COLUMNS TO THE BOARD
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
-
-// CREATE NEW CARDS
-var card1 = new Card("New task");
-var card2 = new Card("Create Kanban board");
-
-// ADD CARDS TO COLUMNS
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
